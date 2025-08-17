b154e8bcaa244519603017582e1967aef04679d4
Lia Moua <moua0061@yahoo.com>
2025-08-16 23:08:41 -0400

    add profile entity

---
+++ src/main/java/com/springbootbasics/store/entities/Profile.java
@@ -0,0 +1,28 @@
+package com.springbootbasics.store.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "profiles")
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "date_of_birth")
+    private String dateOfBirth;
+
+    @Column(name = "loyalty_points")
+    private int loyaltyPoints;
+}
