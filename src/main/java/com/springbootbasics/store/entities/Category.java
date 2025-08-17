9240f4db1e15e1af1424fb97c111798810f1b18a
Lia Moua <moua0061@yahoo.com>
2025-08-17 00:04:43 -0400

    added product & category entities

---
+++ src/main/java/com/springbootbasics/store/entities/Category.java
@@ -0,0 +1,21 @@
+package com.springbootbasics.store.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Product> products = new HashSet<>();
+}
